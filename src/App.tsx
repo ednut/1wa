fdf20655d4dbd825e45bd9f5b57cf8f4613f20cd
Babatunde Adegbite <babatunde.adeyemi@izea.com>
2021-04-07 20:15:04 +0100

    added json data

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import Data from './friendsList.json';
 
 function App() {
   return (
     <div>
       <h1>I'm new here</h1>
+      <div>
+        {Data.map((post) => {
+          return (
+            <div key={post.id}>
+              <h1> {post.title} </h1>
+              <p> {post.socialHandle} </p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
